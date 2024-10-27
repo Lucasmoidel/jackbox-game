31312ffd37bafe9637bdc7280a977e001d311dfc
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-10-26 18:19:37 -0700

    Update main.gd

--- main.gd
+++ main.gd
@@ -1,10 +1,17 @@
 extends Control
 
-
+var roomId: String
+var players = []
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	for i in range(json.number):
-		print(json.people[i].name)
+	roomId = json.room.ID
+	print(roomId)
+	for i in range(json.players):
+		players.append(json.room.player[i])
+	print (players)
+	if json.Acknowledge == True:
+		print("Acknowledge")
+	
 
 
 func _on_button_pressed() -> void:
